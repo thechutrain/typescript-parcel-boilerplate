4223d7d49b88fd903ec5de636a77b49e886041f8
thechutrain <chu.chuyalan@gmail.com>
2018-10-29 12:35:15 -0400

    initial tsconfig options

---
+++ src/main.ts
@@ -0,0 +1,13 @@
+class Person{
+	name: string;
+	age: number;
+	constructor(name, age) {
+		this.name = name;
+		this.age = age;
+	}
+
+	describeMe(){
+		console.log(`Hi, my name is ${this.name} and I am ${this.age} years old`);
+	}
+}
+
