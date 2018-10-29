5748d1d5917a0567cff0ac42f514cc6eb4241e46
thechutrain <chu.chuyalan@gmail.com>
2018-10-29 16:10:27 -0400

    jest vscode extension working

--- __tests__/Person.test.ts
+++ __tests__/Person.test.ts
@@ -3,6 +3,6 @@ import Person, { randomAge } from '../src/Person';
 describe('Person test', () => {
 	it('should be able to create a person', () => {
 		const bob = new Person('bob', 23);
-		expect(bob.name).toBe('bobo');
+		expect(bob.name).toBe('bob');
 	});
 });
